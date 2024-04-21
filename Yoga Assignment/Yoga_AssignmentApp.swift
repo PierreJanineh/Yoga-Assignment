822e91b14dfa43165fa4de6d6f47264650c008c6
Pierre Janineh <janinehpierre@gmail.com>
2024-04-21 10:55:42 +0300

    Initial Commit

---
+++ Yoga Assignment/Yoga_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yoga_AssignmentApp.swift
+//  Yoga Assignment
+//
+//  Created by Pierre Janineh on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Yoga_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
